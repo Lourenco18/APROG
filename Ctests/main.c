47f1e1c60111c55df640d517f01daafc72dd4d6c
Daniel Lourenço <153820691+Lourenco18@users.noreply.github.com>
2024-09-23 19:48:01 +0100

    ficha1

--- Ctests/main.c
+++ Ctests/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+int number = 16;
 int main() {
-
-printf("Hello, World!\n");
-printf("My name is Daniel!\n");
+if (number == 15)
+{
+   printf("Number is 15\n");
+}else
+{
+    printf("Number is not 15\n");
+}
+return 0;
 
 }
