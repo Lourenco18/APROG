9fcc39ec478a80b248303ffd31169683908db160
Daniel Lourenço <153820691+Lourenco18@users.noreply.github.com>
2024-09-30 16:44:41 +0100

    a

---
+++ Ctests/Ficha1/ex6.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+
+int main() {
+    int diaria = 60;
+    float taxa_maior_15 = 5.50;
+    float taxa_8_15 = 6;
+    float taxa_menor_8 = 8;
+    int dias;
+    char nome[50];
+    float valor_final = 0;
+
+  printf("Qual é o seu nome? ");
+    scanf("%s", nome);
+    
+    // Entrada de dados
+    printf("Quantos dias esteve no hotel? ");
+    scanf("%d", &dias);
+    
+  
+    
+    // Verificação dos dias e cálculo do valor final
+    if (dias > 0) {
+        if (dias > 15) {
+            valor_final = dias * diaria + taxa_maior_15 * dias;
+        } else if (dias >= 8 && dias <= 15) {
+            valor_final = dias * diaria + taxa_8_15 * dias;
+        } else if (dias < 8) {
+            valor_final = dias * diaria + taxa_menor_8 * dias;
+        }
+
+        // Exibir o resultado
+        printf("Nome: %s\n", nome);
+        printf("Valor final: %.2f\n", valor_final);
+    } else {
+        printf("Valor inválido\n");
+    }
+
+    return 0;
+}
